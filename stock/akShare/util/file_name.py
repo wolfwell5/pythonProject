a53ce8af8b54cb867f9041573bcc32eaebc8888e
wolfwell5 <416129082@qq.com>
2026-02-19 11:14:44 +0800

    save data to invoker folder

--- stock/akShare/util/file_name.py
+++ stock/akShare/util/file_name.py
@@ -4,9 +4,6 @@ from datetime import datetime
 from pathlib import Path
 
 
-# absolute_path = 'E:/Develop/Repos/pythonProject/stock/akShare/sample_data/'
-
-
 def get_caller_filename_absolute_path():
     # 获取调用栈的上一帧（即调用该函数的上下文）
     frame = inspect.currentframe().f_back.f_back.f_back
@@ -20,17 +17,10 @@ def get_caller_filename_absolute_path():
 def generate_file_name(core_word) -> str:
     class_name = get_caller_filename_absolute_path()
     path_obj = Path(class_name)
-    # E:\Develop\Repos\pythonProject\stock\akShare
-    akshare_base_path = path_obj.parent.parent  # 获取"北交所"目录的父目录
-
-    # 转换为字符串
-    # base_path_str = str(akshare_base_path)
+    akshare_base_path = path_obj.parent  # E:\Develop\Repos\pythonProject\stock\akShare\十大流通股东
 
     fetched_data_path = f'{akshare_base_path}/sample_data/'
-    # file_name, ext = os.path.splitext(os.path.basename(class_name))
     parent_dir = os.path.basename(os.path.dirname(class_name))
-    # file_parent_folder_name = os.path.basename(os.path.dirname(class_name))
-    # folder_path = f'{fetched_data_path}{parent_dir}/{file_name}'
     folder_path = f'{fetched_data_path}{parent_dir}/'
     file_name = f'{datetime.now().strftime("%Y-%m-%d-%H-%M")}_{core_word}.csv'
 
