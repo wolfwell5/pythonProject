1a2ad36a9261df3d91d4b2bde6bf9a9fc20948eb
wolfwell5 <416129082@qq.com>
2026-03-16 11:37:19 +0800

    加注释

--- stock/akShare/util/csv.py
+++ stock/akShare/util/csv.py
@@ -1,7 +1,11 @@
 from stock.akShare.util.file_name import generate_file_name
 
 
+# 生成csv文件 到当前执行程序的同一目录下
 def generate_csv(data_frame, file_name, specific_folder):
     full_path_csv = generate_file_name(file_name, specific_folder)
     data_frame.to_csv(full_path_csv, index=False)
     print(f'生成csv文件完毕， 路径：  {full_path_csv}')
+
+
+# append 数据到csv文件中 todo
